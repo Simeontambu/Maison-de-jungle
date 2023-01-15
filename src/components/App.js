157909810a2fac9a7b2181a5a226720a7c617bb1
simeontambu <simeontambu@gmail.com>
2023-01-15 15:43:25 +0100

    Deuxième

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import Banner from './Banner'
 import logo from '../assets/logo.png'
-// import Cart from './Cart'
+import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
+import '../styles/Layout.css'
 
 function App() {
 	return (
@@ -12,8 +13,10 @@ function App() {
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
 
-			{/* <Cart /> */}
-			<ShoppingList />
+			<div className='lmj-layout-inner'>
+				<Cart />
+				<ShoppingList />
+			</div>
 			<Footer />
 		</div>
 	)
